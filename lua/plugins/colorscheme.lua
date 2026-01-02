b3ea572a84104c1bce99f0af2fc7651a2721f860
Nalwa-Jayesh <nalwajayesh97@gmail.com>
2026-01-02 12:16:49 +0530

    fix: finally a colorscheme I am satisfied with

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,34 @@
 return {
-  {
-    "typicode/bg.nvim",
-    lazy=false,
-  },
-  {
+	{
+		"typicode/bg.nvim",
+		lazy = false,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				compile = true,
+				undercurl = true,
+				transparent = false,
+				theme = "dragon",
+			})
+			vim.cmd("colorscheme kanagawa-dragon")
+		end,
+	},
+	--[[{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},]]
+	--[[ {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
       vim.cmd("colorscheme rose-pine")
     end,
   }
+  ]]
 }
