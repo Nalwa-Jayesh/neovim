5dd972d5c2c00eeb484e1b0abb15ab27988079ea
Nalwa-Jayesh <nalwajayesh97@gmail.com>
2026-03-05 20:42:21 +0530

    fix: Fixed the Ruff formatting for the projects

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,18 +5,30 @@ return {
 		local conform = require("conform")
 
 		conform.setup({
+			formatters = {
+				ruff = {
+					command = "ruff",
+					args = {
+						"format",
+						"--stdin-filename",
+						"$FILENAME",
+						"-",
+					},
+					stdin = true,
+				},
+			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff" },
-				javascript = { "prettierd", "prettier" },
-				typescript = { "prettierd", "prettier" },
-				json = { "prettierd", "prettier" },
-				yaml = { "prettierd", "prettier" },
-				markdown = { "prettierd", "prettier" },
+				javascript = { "prettier", "prettierd", stop_after_first = false },
+				typescript = { "prettier", "prettierd", stop_after_first = false },
+				json = { "prettier", "prettierd", stop_after_first = false },
+				yaml = { "prettier", "prettierd", stop_after_first = false },
+				markdown = { "prettier", "prettierd", stop_after_first = false },
 				golang = { "gofumpt" },
 			},
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 5000,
 				lsp_fallback = true,
 			},
 		})
