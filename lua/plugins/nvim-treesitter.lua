bcf0347246e46457251fe72109a4960c064a366e
Nalwa-Jayesh <nalwajayesh97@gmail.com>
2026-03-05 20:44:32 +0530

    fix: fixed the treesitter and textobjects

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,31 +1,86 @@
 return {
+	-- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
+	event = "VeryLazy",
 	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	opts = {
 		ensure_installed = {
+			"bash",
 			"c",
+			"diff",
+			"html",
 			"lua",
-			"vim",
-			"vimdoc",
-			"query",
+			"luadoc",
 			"markdown",
 			"markdown_inline",
-			"python",
-			"dockerfile",
-			"toml",
-			"bash",
+			"query",
+			"vim",
+			"vimdoc",
+			"rust",
+			"regex",
+			"php",
+			"blade",
 		},
-		auto_install = false,
+		-- Autoinstall languages that are not installed
+		auto_install = true,
 		highlight = {
 			enable = true,
-			disable = function(_, buf)
-				local max_filesize = 100 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-			additional_vim_regex_highlighting = false,
+			-- Some languages depend on vim"s regex highlighting system (such as Ruby) for indent rules.
+			--  If you are experiencing weird indenting issues, add the language to
+			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+			additional_vim_regex_highlighting = { "ruby" },
+		},
+		indent = { enable = true, disable = { "ruby" } },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<M-space>", -- set to `false` to disable one of the mappings
+				node_incremental = "<M-space>",
+				scope_incremental = false,
+				node_decremental = "<Backspace>",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+
+				-- Automatically jump forward to textobj, similar to targets.vim
+				lookahead = true,
+
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
+					["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+					["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					-- You can also use captures from other query groups like `locals.scm`
+					["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>xs"] = {
+						query = "@parameter.inner",
+						desc = "Swap the node under the cursor with the next",
+					},
+				},
+				swap_previous = {
+					["<leader>xS"] = {
+						query = "@parameter.inner",
+						desc = "swap the node under the cursor with the previous",
+					},
+				},
+			},
 		},
 	},
+	-- There are additional nvim-treesitter modules that you can use to interact
+	-- with nvim-treesitter. You should go explore a few and see what interests you:
+	--
+	--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
